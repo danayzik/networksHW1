f767a093da962713196ec41c7bec38ac811e94b0
danayzik <danayzik@mail.tau.ac.il>
2024-11-11 20:30:54 +0200

    initial commit

---
+++ numbers_server.py
@@ -0,0 +1,17 @@
+import socket
+import sys
+from typing import List
+
+
+def main(args: List[str]):
+    pass
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    input_args = sys.argv
+    main(input_args)
